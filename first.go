d0025f623eefe2d46d0e8fe3a3c742fb51a4ccae
phoenix-force <paradox.joe.007@gmail.com>
2021-05-26 23:30:39 +0530

    first commit

---
+++ first.go
@@ -0,0 +1,55 @@
+package main
+
+import (  
+    "fmt"
+)
+
+func main() {  
+    // fmt.Println("Hello World")
+    // name:= "Paradox"
+    // var age int = 25
+    // a, b := 145.8, 543.8
+    // var c = math.Min(a,b)
+
+    // fmt.Println("my name is", name,"a=>", a, "b=>", b, "min=>", c ," and my age is", age)
+
+    // c1 := complex(5, 7)
+    // c2 := 8 + 27i
+    // cadd := c1 + c2
+    // fmt.Println("sum:", cadd)
+    // cmul := c1 * c2
+    // fmt.Println("product:", cmul)
+
+
+    // first := "D"
+    // last := "dox"
+    // name := first +" "+ last
+    // fmt.Println("My name is",name)
+
+
+    // i := 55      //int
+    // j := 67.8    //float64
+    // sum := float64(i) + j //int + float64 not allowed
+    // fmt.Println(sum)
+
+
+    // var defaultName string = "Sam" //allowed
+    // type myString string
+    // var customName myString = "Sam" //allowed
+    // customName = defaultName //not allowed
+
+
+
+    // const a = 5
+    // var intVar int = a
+    // var int32Var int32 = a
+    // var float64Var float64 = a
+    // var complex64Var complex64 = a
+    // fmt.Println("intVar",intVar, "\nint32Var", int32Var, "\nfloat64Var", float64Var, "\ncomplex64Var",complex64Var)
+
+    var a = 40.2 % 8
+    fmt.Printf("a's type is %T and value is %v", a, a)
+
+
+
+}
